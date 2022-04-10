899f127e33e76e50cb10edbbcb09f7ba24b9c74a
d-emrys <omayr010@gmail.com>
2022-04-10 11:42:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-xy7gje
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-xy7gje)
---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  const[counter setCounter]=
+  useState(0);
+}
+function increment{setCounter(counter+1);}
